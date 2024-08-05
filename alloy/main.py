0119f5d8e5deb1f048a8320c8bc7c9c56c8acd7e
olli <144932831+OLILHR@users.noreply.github.com>
2024-08-05 15:18:46 +0200

    ✨ Prompt for input, output and filters in case no paths are provided (#6)
    * Prompt for input, output and filters in case no path is provided
    
    * Fix encoding error during consolidation
    
    * Add input/output path flags
    
    * Use lazy logging and more specific file error handling
    
    * Fix formatting
    
    * Add default filter
    
    * Fix ignore list's implicit-str-concat

--- alloy/main.py
+++ alloy/main.py
@@ -14,26 +14,42 @@ _logger.setLevel(GLOBAL_LOG_LEVEL)
 
 
 @click.command()
-@click.argument("path", type=click.Path(exists=True))
+@click.option("-i", "--input-path", type=click.Path(exists=True), help="Input path for the codebase")
+@click.option("-o", "--output-path", type=click.Path(), help="Output path for the generated markdown")
 @click.option(
     "--filter",
     "-f",
     "extensions",
     callback=parse_extensions,
     multiple=True,
-    help="Filter files by extension via an optional '-f' flag, for instance: -f py,json,yml",
+    help="OPTIONAL FILTERING BY EXTENSIONS; FOR INSTANCE: -f py,json",  # consolidates only .py and .json files
 )
-def generate_markdown(path, extensions):
+def generate_markdown(input_path, output_path, extensions):
+    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    current_dir = os.getcwd()
+
+    if input_path is None:
+        input_path = click.prompt("INPUT PATH:", type=click.Path(exists=True), default=current_dir)
+    else:
+        input_path = os.path.abspath(os.path.join(current_dir, input_path))
+
+    if output_path is None:
+        output_path = click.prompt("OUTPUT PATH:", type=click.Path(), default=project_root)
+    else:
+        output_path = os.path.abspath(os.path.join(current_dir, output_path))
+
     extensions = list(extensions) if extensions else None
-    markdown_content = consolidate(path, extensions)
-    project_root = os.path.dirname(os.path.abspath(__file__))
-    output_file = os.path.join(project_root, "../codebase.md")
+
+    markdown_content = consolidate(input_path, extensions)
+    output_file = os.path.join(output_path, "alloy.md")
+
+    os.makedirs(output_path, exist_ok=True)
 
     with open(output_file, "w", encoding="utf-8") as f:
         f.write(markdown_content)
 
-    _logger.info("Markdown file generated at %s", output_file)
+    _logger.info("CODEBASE CONSOLIDATED AT %s", output_file)
 
 
 if __name__ == "__main__":
-    generate_markdown()  # pylint: disable=no-value-for-parameter
+    generate_markdown.main()
